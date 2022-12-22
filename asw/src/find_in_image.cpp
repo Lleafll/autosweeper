8a3d8dae58c483cb3c36fdfe3540e64106e2aa98
Lars Lorenz <lars.ll.lorenz@gmail.com>
2022-12-22 15:14:32 +0100

    Fix out-of-bounds memory access

--- asw/src/find_in_image.cpp
+++ asw/src/find_in_image.cpp
@@ -12,16 +12,21 @@ namespace asw {
 std::vector<Position>
 find_in_image(ImageConstSpan const& image, ImageConstSpan const& sub_image) {
     std::vector<Position> positions;
-    for (auto row = 0; row < image.extent(0); ++row) {
-        for (auto column = 0; column < image.extent(1); ++column) {
-            if (equals(stdex::submdspan(
-                               image,
-                               std::pair{row, row + sub_image.extent(0)},
-                               std::pair{column, column + sub_image.extent(1)}),
-                       sub_image)) {
-                positions.push_back(
-                        {gsl::narrow_cast<size_t>(row),
-                         gsl::narrow_cast<size_t>(column)});
+    auto const sub_height = sub_image.extent(0);
+    auto const sub_width = sub_image.extent(1);
+    if (sub_image.size() > 0) {
+        for (auto row = 0; row + sub_height <= image.extent(0); ++row) {
+            for (auto column = 0; column + sub_width <= image.extent(1);
+                 ++column) {
+                if (equals(stdex::submdspan(
+                                   image,
+                                   std::pair{row, row + sub_height},
+                                   std::pair{column, column + sub_width}),
+                           sub_image)) {
+                    positions.push_back(
+                            {gsl::narrow_cast<size_t>(row),
+                             gsl::narrow_cast<size_t>(column)});
+                }
             }
         }
     }
